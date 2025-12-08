6c65db87c830387dd8b84b9de5369aeb0bef86e6
Aditya Gupta <realadityagupta2@gmail.com>
2025-12-08 16:41:10 +0530

    extraction method added

--- youtube/transcriptyt.py
+++ youtube/transcriptyt.py
@@ -2,10 +2,11 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import json
 
 # ask user for youtube id
-youtube_id = "TXHF6_OS-tc"  # input("enter the youtube url")
+youtube_id = input("enter the youtube url")
 # here is youtube link:-https://www.youtube.com/watch?v=-YeNbBi30oo
+youtube_id = youtube_id.split("=")
 ytt_api = YouTubeTranscriptApi()
-response = ytt_api.fetch(youtube_id)
+response = ytt_api.fetch(youtube_id[1])
 # print(response.snippets)
 transcript =""
 for i in response.snippets:
